4f4a8eaed536692e4d1afd4af40cd9cc9e9ebff5
amyspark-ng ✨ <92493175+amyspark-ng@users.noreply.github.com>
2024-08-30 00:20:48 -0500

    delete unnecesary files

--- source/loader.ts
+++ source/loader.ts
@@ -176,7 +176,6 @@ function loadAllSprites() {
 	})
 
 	loadSprite("part_star", "sprites/part_star.png")
-	loadSprite("osaka", "sprites/osaka.png")
 	loadSprite("panderito", "sprites/panderito.png")
 	loadSprite("smallpanderito", "sprites/smallpanderito.png")
 	loadSprite("folderObj", "sprites/folderObj.png")
@@ -399,84 +398,6 @@ function loadAllSprites() {
 	loadSprite("upgrade", "sprites/windows/storeWin/upgrade.png")
 	loadSprite("upgradelock", "sprites/windows/storeWin/upgradelock.png")
 
-	loadSprite("upgrades", "sprites/upgrades.png", {
-		sliceX: 18,
-		sliceY: 3,
-		anims: {
-			"k_0": {
-				from: 0,
-				to: 2,
-				
-			},
-			"k_1": {
-				from: 3,
-				to: 5,
-			},
-			"k_2": {
-				from: 6,
-				to: 8,
-			},
-			"k_3": {
-				from: 9,
-				to: 11,
-			},
-			"k_4": {
-				from: 12,
-				to: 14,
-			},
-			"k_5": {
-				from: 15,
-				to: 17,
-			},
-			"c_6": {
-				from: 18,
-				to: 20,
-			},
-			"c_7": {
-				from: 21,
-				to: 23,
-			},
-			"c_8": {
-				from: 24,
-				to: 26,
-			},
-			"c_9": {
-				from: 27,
-				to: 29,
-			},
-			"c_10": {
-				from: 30,
-				to: 32,
-			},
-			"c_11": {
-				from: 33,
-				to: 35,
-			},
-		}
-	})
-
-	loadSprite("mupgrades", "sprites/mupgrades.png", {
-		sliceX: 12,
-		sliceY: 1,
-		anims: {
-			"u_12": {
-				from: 0,
-				to: 2,
-			},
-			"u_13": {
-				from: 3,
-				to: 5,
-			},
-			"u_14": {
-				from: 6,
-				to: 8,
-			},
-			"u_15": {
-				from: 9,
-				to: 11,
-			},
-		}
-	})
 	//#endregion Store
 
 	//#region Music
